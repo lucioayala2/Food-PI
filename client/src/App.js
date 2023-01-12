2d033556c6d6e29caef857bd0933a3c0908cccbb
lucioayala2 <lucioayala97@gmail.com>
2023-01-11 21:31:50 -0300

    first App settings done for Main component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+
 import './App.css';
+import {Redirect, Route} from 'react-router-dom';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <Route path='/' exact render={() => <Main />} />
+      <Redirect to='/' />
     </div>
   );
 }
---
+++ client/src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Main(){
+        return(
+            <div>
+                <h1>LUCIO FOOD PI</h1>
+            </div>
+        )
+    }
